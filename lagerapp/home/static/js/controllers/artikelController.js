ddab71106f00bb1d788306d093053ab982eb3dd7
Matthias Lenz <lenzmtt@gmail.com>
2015-11-09 19:32:52 +0100

    Add Files

---
+++ lagerapp/home/static/js/controllers/artikelController.js
@@ -0,0 +1,89 @@
+angular.module('artikelApp').controller('ArtikelCtrl', function($scope,$resource,Nature) {  
+  window.scope=vm;
+  var vm = this;
+  vm.ordering= "id";
+  vm.page = 1;
+  vm.perPage = 20;
+  vm.query = '';
+  vm.resourcenatureid = null;
+  var product = $resource(
+    '/api/product/:id',
+	{per_page:20,q:""},
+    {query: {method:'GET', isArray:false}}
+  );
+  var nature = Nature.query({}, function() {
+	  vm.resourcenatureids = [{"id":"", "name":"- Bitte auswählen -", "title":""}];
+	  var titleDescr = "";
+	  nature.forEach(function (item, index, array) {
+		if (item.title){
+			titleDescr=item.name;
+		}
+		else{
+			vm.resourcenatureids.push({"id":item.id, "name":item.name, "title":titleDescr});
+		}
+	  });
+  });
+  vm.test1 = Nature.get({'id': '90230000'});
+   
+  vm.updateList = function(resetPage){
+    /*var query = vm.query+" in:title repo:angular/angular.js";*/
+    var query = vm.query;
+    vm.page = resetPage ? 1 : vm.page;
+    product.query({
+		ordering:vm.ordering,
+		page:vm.page,
+		page_size:vm.perPage,
+		search:query,
+		resourcenatureid:  vm.resourcenatureid
+    }, function(data){
+      vm.items = data.results;
+      vm.lastPage = Math.ceil(data.count/vm.perPage);
+    });
+  };
+  $scope.$watchCollection('[artikel.page,artikel.ordering]',function(){
+    vm.updateList(false );
+  });
+  $scope.$watchCollection('[artikel.query,artikel.resourcenatureid,artikel.perPage]',function(){
+    //search and filter operations change the resulting size
+    vm.updateList(true);
+  });
+
+  vm.nextPage = function(){
+    if (vm.lastPage !== vm.page){
+      vm.page++;
+    }
+  };
+  vm.previousPage = function(){
+    if (vm.page !== 1){
+      vm.page--;
+    }
+  };
+  vm.setPage = function(num){
+      if (num <=vm.lastPage && num>0){
+		vm.page=num;
+	 }
+  };
+  vm.setOrder = function(field){
+    if (vm.ordering == field){
+      vm.ordering= '-'+vm.ordering;
+	 vm.sortorder = 'desc';
+    }
+    else {
+	 vm.ordering=field;  
+	 vm.sortorder = 'asc';
+    }
+  };
+  vm.getOrder = function(){
+	if (vm.ordering.charAt(0)=='-'){
+		return vm.ordering.substr(1, vm.ordering.length);
+	}
+	else return vm.ordering;
+  };
+  vm.sortDirection = function(field){
+    if (vm.ordering.charAt(0)=='-'){
+      return 'sort-caret desc';
+    } else return 'sort-caret asc';
+  };  
+  
+
+ });
---
+++ lagerapp/home/static/js/controllers/baseController.js
@@ -0,0 +1,16 @@
+angular.module('artikelApp').controller('BaseCtrl', function() {  
+  var vm = this;
+  vm.title = 'Artikel Stammdaten';
+  vm.tabs = [	{title: 'Artikel Stammdaten', url: 'artikel'}, 
+				{title: 'Bestellmodul', url: 'bestellmodul'}, 
+				{title: 'Lagereingang', url: 'lagereingang'}, 
+				{title: 'Lagerausgang', url: 'lagerausgang'}, 
+				{title: 'Lagerverwaltung', url: 'lagerverwaltung'},
+				{title: 'Verrechnungen', url: 'verrechnungen'}];    
+  vm.currentTab = 'artikel';
+  vm.onClickTab = function (tab) { 
+		vm.currentTab = tab.url; 
+		vm.title = tab.title; 
+	}
+  vm.isActiveTab = function(tabUrl) { return tabUrl == vm.currentTab; }
+ });
