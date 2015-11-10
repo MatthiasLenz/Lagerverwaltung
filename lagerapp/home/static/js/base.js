aabd90c2871835967bfa3f49e3582880fa6c06b8
Matthias Lenz <lenzmtt@gmail.com>
2015-11-10 11:52:30 +0100

    Refactor (code formatting)

--- lagerapp/home/static/js/base.js
+++ lagerapp/home/static/js/base.js
@@ -1,70 +1,76 @@
-angular.module('artikelApp', ['ui.router','ngResource'])
+angular.module('artikelApp', ['ui.router', 'ngResource']).
 
-.config(["$locationProvider", function($locationProvider) {
-  //disable this, if the app is being used by html5 incompatible browsers.
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
-}])
-
-.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/artikel");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('artikel', {
-      url: '/artikel',
-      templateUrl: 'static/html/articlelist.html',
-	  controller: 'ArtikelCtrl',
-	  controllerAs: 'artikel',
-      persist: true
-    })
-    .state('bestellmodul', {
-      url: '/bestellmodul',
-      templateUrl: 'static/html/bestellmodul.html',
-      controller: 'BestellungCtrl',
-	  controllerAs: 'bst'
-    })
-    .state('lagereingang', {
-      url: '/lagereingang',
-      templateUrl: 'static/html/lagereingang.html',
-    })
-    .state('lagerausgang', {
-      url: '/lagerausgang',
-      templateUrl: 'static/html/lagerausgang.html',
-    })    
-	.state('lagerverwaltung', {
-      url: '/lagerverwaltung',
-      templateUrl: 'static/html/lagerverwaltung.html',
-    })
-	.state('verrechnungen', {
-      url: '/verrechnungen',
-      templateUrl: 'static/html/verrechnungen.html',
+config(["$locationProvider", function ($locationProvider) {
+    //disable this, if the app is being used by html5 incompatible browsers.
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
     });
-})
+}]).
 
-.factory("Nature", function ($resource) {
-	return $resource(
-	  "/api/nature/:id", {id: "@id" },{"reviews": {'method': 'GET', 'params': {'reviews_only': "true"}, isArray: true}}
+config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/artikel");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('artikel', {
+            url: '/artikel',
+            templateUrl: 'static/html/articlelist.html',
+            controller: 'ArtikelCtrl',
+            controllerAs: 'artikel',
+            persist: true
+        })
+        .state('bestellmodul', {
+            url: '/bestellmodul',
+            templateUrl: 'static/html/bestellmodul.html',
+            controller: 'BestellungCtrl',
+            controllerAs: 'bst'
+        })
+        .state('lagereingang', {
+            url: '/lagereingang',
+            templateUrl: 'static/html/lagereingang.html',
+        })
+        .state('lagerausgang', {
+            url: '/lagerausgang',
+            templateUrl: 'static/html/lagerausgang.html',
+        })
+        .state('lagerverwaltung', {
+            url: '/lagerverwaltung',
+            templateUrl: 'static/html/lagerverwaltung.html',
+        })
+        .state('verrechnungen', {
+            url: '/verrechnungen',
+            templateUrl: 'static/html/verrechnungen.html',
+        });
+}).
+
+factory("Nature", function ($resource) {
+    return $resource(
+        "/api/nature/:id", {id: "@id"}, {
+            "reviews": {
+                'method': 'GET',
+                'params': {'reviews_only': "true"},
+                isArray: true
+            }
+        }
     );
-}); 
+});
 
- /*    How to transform a query to a paginated resource, so that it returns the results directly
-artikelApp.factory("Product", 
-	function ($resource) {
-		return $resource("/api/product/:id", {},{
-		   //override .query 
-		   query: {
-                method: 'GET',
-                url: 'api/product/',
-                isArray: true,
-                transformResponse: function(data, headers) {
-                    return angular.fromJson(data).results;
-                },
-            }, 
-		});
-	}
-); */
+/*    How to transform a query to a paginated resource, so that it returns the results directly
+ artikelApp.factory("Product",
+ function ($resource) {
+ return $resource("/api/product/:id", {},{
+ //override .query
+ query: {
+ method: 'GET',
+ url: 'api/product/',
+ isArray: true,
+ transformResponse: function(data, headers) {
+ return angular.fromJson(data).results;
+ },
+ },
+ });
+ }
+ ); */
--- lagerapp/home/static/js/controllers/bestellungController.js
+++ lagerapp/home/static/js/controllers/bestellungController.js
@@ -1,12 +1,13 @@
-angular.module('artikelApp').controller('BestellungCtrl', function ($scope, $resource) {
+angular.module('artikelApp').
+controller('BestellungCtrl', function ($scope, $resource) {
     var vm = this;
     vm.article = "";
-    vm.supplier="";
-    vm.date="";
-    vm.amount="";
-    vm.projectid="";
-    vm.baustelle="Musterbaustelle";
-    vm.bauleiter="Mustermann";
+    vm.supplier = "";
+    vm.date = "";
+    vm.amount = "";
+    vm.projectid = "";
+    vm.baustelle = "Musterbaustelle";
+    vm.bauleiter = "Mustermann";
     var product = $resource(
         '/api/product/:id',
         {per_page: 20, q: ""},
