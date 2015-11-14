e0f728f17bc41f3be493d5f55b77957b56d8fd12
Matthias Lenz <lenzmtt@gmail.com>
2015-11-14 18:12:41 +0100

    show suppliers for product in step2

--- lagerapp/home/static/bestellen/controllers/Step2Ctrl.js
+++ lagerapp/home/static/bestellen/controllers/Step2Ctrl.js
@@ -1,14 +1,23 @@
 angular.module('baseApp.bestellen').
-controller('Step2Ctrl', ['$scope', '$injector', function ($scope, $injector) {
+controller('Step2Ctrl', ['$http', '$scope', function ($http, $scope) {
     // 1. Self-reference
     var controller = this;
-
     // 2. requirements
     // 3. Do scope stuff
     // 3a. Set up watchers on the scope.
     // 3b. Expose methods or data on the scope
     // 3c. Listen to events on the scope
     // 4. Expose methods and properties on the controller instance
+    controller.product = $scope.bestellen.selected;
+    controller.suppliers = [];
     // 5. Clean up
-    // 6. All the actual implementations go here.
+    // 6. All the actual implementations go here
+    controller.product.supplier.forEach(
+        function (entry) {
+            $http.get(entry).then(function (response) {
+                controller.suppliers.push(response.data);
+            });
+        }
+    );
+
 }]);
