4dc419d189e83258a093a2ce4d851e647d0b91e4
Matthias Lenz <lenzmtt@gmail.com>
2015-11-18 20:33:00 +0100

    purchaseorder detail

--- lagerapp/home/static/bestellungen/directives/bestellungen.js
+++ lagerapp/home/static/bestellungen/directives/bestellungen.js
@@ -4,7 +4,19 @@ directive('bestellungen', function () {
         templateUrl: 'static/bestellungen/directives/bestellungen.html',
         controller: ['$scope', 'bestellungenService', function ($scope, bestellungenService) {
             var controller = this;
-            controller.list = bestellungenService.resource.query({'status': 0});
+            controller.list = [];
+            controller.showDetail = {};
+            bestellungenService.resource.query({'status': 0}).$promise.then(function (result) {
+                result.forEach(function (item) {
+                    controller.list.push(item);
+                    controller.showDetail[item.id] = false;
+                });
+            });
+
+
+            controller.toggleDetail = function (id) {
+                controller.showDetail[id] = !(controller.showDetail[id]);
+            };
         }],
         controllerAs: 'bestellungen'
     };
