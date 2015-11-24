50e3c6736b2bf6b4ca1692e6f41b8f92811e6711
Matthias Lenz <lenzmtt@gmail.com>
2015-11-24 21:11:14 +0100

    change model productdocdata to unmanaged

--- lagerapp/masterdata/models.py
+++ lagerapp/masterdata/models.py
@@ -195,7 +195,7 @@ class PurchaseDocData(models.Model):
     amount = models.FloatField(db_column='Amount', blank=True, null=True)
 
     class Meta:
-        managed = True
+        managed = False
         db_table = 'PurchaseDocData'
         app_label = 'hit_01_purchase'
 
