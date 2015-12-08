bce434275795bacb861aecf79678e6b1e2701e9f
Matthias Lenz <lenzmtt@gmail.com>
2015-12-08 22:57:31 +0100

    Frontend display deliverynotedata and mark rows with the same product id

--- lagerapp/home/static/bestellungen/directives/bestellungen1.js
+++ lagerapp/home/static/bestellungen/directives/bestellungen1.js
@@ -7,7 +7,11 @@ directive('bestellungen1', function () {
             controller.list = [];
             updateList();
             controller.showDetail = {};
+            controller.marked = "";
             controller.select = null;
+            controller.mark = function (prodid) {
+                controller.marked = prodid;
+            };
             controller.edit_doc = function (doc) {
                 var temp = doc.edit;
                 controller.list.forEach(function (item) {
@@ -23,6 +27,15 @@ directive('bestellungen1', function () {
             function updateList() {
                 controller.list = [];
                 bestellungenService.purchasedoc.list({'status': 1}).then(function (result) {
+                    result.forEach(function (item) {
+                        supplier = supplierService.resource.query({'id': item.supplierid});
+                        item.supplier = supplier;
+                        controller.list.push(item);
+                        controller.showDetail[item.id] = false;
+                    });
+                });
+                //ToDo refactoring, doppelter code
+                bestellungenService.purchasedoc.list({'status': 2}).then(function (result) {
                     result.forEach(function (item) {
                         supplier = supplierService.resource.query({'id': item.supplierid});
                         item.supplier = supplier;
