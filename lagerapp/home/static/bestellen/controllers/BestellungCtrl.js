387c3dfcaba35688a4dee68f7fe2e8ba0a33e3ed
Matthias Lenz <mttlnz@gmail.com>
2016-06-02 22:32:35 +0200

    Refactoring

--- lagerapp/home/static/bestellen/controllers/BestellungCtrl.js
+++ lagerapp/home/static/bestellen/controllers/BestellungCtrl.js
@@ -1,28 +1,27 @@
 angular.module('baseApp.bestellen').
 controller('BestellungCtrl', [function () {
-    var ctrl = this;
+    var vm = this;
     this.state = 'best1';
-    window.bestellen = this;
     this.selectprod = function (product) {
-        ctrl.selectedprod = product;
-        ctrl.selectedsupp = null;
-        ctrl.finished = false;
-        ctrl.state = 'best2';
+        vm.selectedprod = product;
+        vm.selectedsupp = null;
+        vm.finished = false;
+        vm.state = 'best2';
     };
     this.selectsupp = function (supplier) {
-        ctrl.selectedsupp = supplier;
-        ctrl.finished = false;
-        ctrl.state = 'best3';
+        vm.selectedsupp = supplier;
+        vm.finished = false;
+        vm.state = 'best3';
     };
     this.finish = function () {
-        ctrl.state = 'best4';
-        ctrl.finished = true;
+        vm.state = 'best4';
+        vm.finished = true;
     }
     this.selectState = function (state) {
         if (state == 'best1' ||
-            (state == 'best2' && ctrl.selectedprod) ||
-            (state == 'best3' && ctrl.selectedsupp) ||
-            (state == 'best4' && ctrl.finished)) {
+            (state == 'best2' && vm.selectedprod) ||
+            (state == 'best3' && vm.selectedsupp) ||
+            (state == 'best4' && vm.finished)) {
             this.state = state;
         }
     }
