2b4e3dd9aecadc96f636aa73fb423b62cbda16e9
Matthias Lenz <mttlnz@gmail.com>
2016-07-13 11:03:41 +0200

    files

---
+++ uninstall.bat
@@ -0,0 +1,18 @@
+pip uninstall pypiwin32==219
+pip uninstall Markdown==2.6.3
+pip uninstall Jinja2==2.7.3
+pip uninstall django-pyodbc-azure==1.8.3.0
+pip uninstall py3o.template==0.9.7
+pip uninstall djangorestframework==3.3.0
+pip uninstall django-filter==0.11.0
+pip uninstall django-profiler==2.0.0
+pip uninstall Django==1.8.3
+pip uninstall Babel==1.3
+
+
+
+
+
+
+
+
